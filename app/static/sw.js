727901a108555939baa4ed836469afb600565b56
timativ-blip <timati_v@inbox.ru>
2026-09-16 08:36:07 +0300

    Rename app to Экзитпол and refresh research illustration and introductory copy

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'exit-poll-v3-turquoise';
+const CACHE = 'exit-poll-v4-exitpoll';
 const FILES = ['/', '/static/app.js', '/static/style.css', '/static/icon.svg', '/static/party-logo.png', '/static/manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('exit-poll-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
